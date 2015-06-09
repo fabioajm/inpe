729aba14640f8a589f8dd0577be5c884dad40179
fabio_ajm <fab.ajm@gmail.com>
2015-06-09 06:13:02 -0300

    refatoração

--- ProjetoFinal/src/main/java/br/inpe/service/CarrinhoComprasService.java
+++ ProjetoFinal/src/main/java/br/inpe/service/CarrinhoComprasService.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.inpe.model.CarrinhoCompras;
+import br.inpe.model.Usuario;
+import br.inpe.observer.AtualizaEstoqueObserver;
+import br.inpe.observer.PreferenciasUsuarioObserver;
 import br.inpe.repository.CarrinhoComprasRepository;
 
 @Service
@@ -15,11 +18,25 @@ public class CarrinhoComprasService {
 	@Autowired
 	private CarrinhoComprasRepository ccRepository;
 	
+	@Autowired
+	private EstoqueService estoqueService;
+	
+	@Autowired
+	private UsuarioService usuarioService;
+	
 	public void save(CarrinhoCompras cc){
 		ccRepository.save(cc);
 	}
 
 	public CarrinhoCompras find(Long id){
 		return ccRepository.findById(id);
+	}
+	public CarrinhoCompras criarCarrinho(Usuario u) {
+		CarrinhoCompras cc;
+		u = usuarioService.find(u.getId());
+		cc = new CarrinhoCompras();
+		cc.adicionarObserver(new AtualizaEstoqueObserver(estoqueService));
+		cc.adicionarObserver(new PreferenciasUsuarioObserver(u));
+		return cc;
 	}
 }
